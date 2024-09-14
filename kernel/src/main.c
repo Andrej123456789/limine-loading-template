48ac52e7fd179e1dcb12bb20e1c762c7beaa5a43
Andrej Bartulin <andrej.bartulinvr@gmail.com>
2024-09-14 18:01:43 +0200

    x86-64-c template

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -1,5 +1,6 @@
-#include <cstdint>
-#include <cstddef>
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
 #include <limine.h>
 
 #include "src/kernel.h"
@@ -8,88 +9,72 @@
 // base revision described by the Limine boot protocol specification.
 // See specification for further info.
 
-namespace {
-
 __attribute__((used, section(".requests")))
-volatile LIMINE_BASE_REVISION(2);
-
-}
+static volatile LIMINE_BASE_REVISION(2);
 
 // The Limine requests can be placed anywhere, but it is important that
 // the compiler does not optimise them away, so, usually, they should
 // be made volatile or equivalent, _and_ they should be accessed at least
 // once or marked as used with the "used" attribute as done here.
 
-namespace {
-
 __attribute__((used, section(".requests")))
-volatile limine_framebuffer_request framebuffer_request = {
+static volatile struct limine_framebuffer_request framebuffer_request = {
     .id = LIMINE_FRAMEBUFFER_REQUEST,
-    .revision = 0,
-    .response = nullptr
+    .revision = 0
 };
 
 __attribute__((used, section(".requests")))
-volatile limine_module_request module_request = {
+static volatile struct limine_module_request module_request = {
     .id = LIMINE_MODULE_REQUEST,
-    .revision = 0,
-    .response = nullptr
+    .revision = 0
 };
-
-}
 
 // Finally, define the start and end markers for the Limine requests.
-// These can also be moved anywhere, to any .cpp file, as seen fit.
-
-namespace {
+// These can also be moved anywhere, to any .c file, as seen fit.
 
 __attribute__((used, section(".requests_start_marker")))
-volatile LIMINE_REQUESTS_START_MARKER;
+static volatile LIMINE_REQUESTS_START_MARKER;
 
 __attribute__((used, section(".requests_end_marker")))
-volatile LIMINE_REQUESTS_END_MARKER;
-
-}
+static volatile LIMINE_REQUESTS_END_MARKER;
 
 // GCC and Clang reserve the right to generate calls to the following
 // 4 functions even if they are not directly called.
 // Implement them as the C specification mandates.
 // DO NOT remove or rename these functions, or stuff will eventually break!
-// They CAN be moved to a different .cpp file.
+// They CAN be moved to a different .c file.
 
-extern "C" {
+void *memcpy(void *dest, const void *src, size_t n) {
+    uint8_t *pdest = (uint8_t *)dest;
+    const uint8_t *psrc = (const uint8_t *)src;
 
-void *memcpy(void *dest, const void *src, std::size_t n) {
-    std::uint8_t *pdest = static_cast<std::uint8_t *>(dest);
-    const std::uint8_t *psrc = static_cast<const std::uint8_t *>(src);
-
-    for (std::size_t i = 0; i < n; i++) {
+    for (size_t i = 0; i < n; i++) {
         pdest[i] = psrc[i];
     }
 
     return dest;
 }
 
-void *memset(void *s, int c, std::size_t n) {
-    std::uint8_t *p = static_cast<std::uint8_t *>(s);
+void *memset(void *s, int c, size_t n) {
+    uint8_t *p = (uint8_t *)s;
 
-    for (std::size_t i = 0; i < n; i++) {
-        p[i] = static_cast<uint8_t>(c);
+    for (size_t i = 0; i < n; i++) {
+        p[i] = (uint8_t)c;
     }
 
     return s;
 }
 
-void *memmove(void *dest, const void *src, std::size_t n) {
-    std::uint8_t *pdest = static_cast<std::uint8_t *>(dest);
-    const std::uint8_t *psrc = static_cast<const std::uint8_t *>(src);
+void *memmove(void *dest, const void *src, size_t n) {
+    uint8_t *pdest = (uint8_t *)dest;
+    const uint8_t *psrc = (const uint8_t *)src;
 
     if (src > dest) {
-        for (std::size_t i = 0; i < n; i++) {
+        for (size_t i = 0; i < n; i++) {
             pdest[i] = psrc[i];
         }
     } else if (src < dest) {
-        for (std::size_t i = n; i > 0; i--) {
+        for (size_t i = n; i > 0; i--) {
             pdest[i-1] = psrc[i-1];
         }
     }
@@ -97,11 +82,11 @@ void *memmove(void *dest, const void *src, std::size_t n) {
     return dest;
 }
 
-int memcmp(const void *s1, const void *s2, std::size_t n) {
-    const std::uint8_t *p1 = static_cast<const std::uint8_t *>(s1);
-    const std::uint8_t *p2 = static_cast<const std::uint8_t *>(s2);
+int memcmp(const void *s1, const void *s2, size_t n) {
+    const uint8_t *p1 = (const uint8_t *)s1;
+    const uint8_t *p2 = (const uint8_t *)s2;
 
-    for (std::size_t i = 0; i < n; i++) {
+    for (size_t i = 0; i < n; i++) {
         if (p1[i] != p2[i]) {
             return p1[i] < p2[i] ? -1 : 1;
         }
@@ -110,17 +95,17 @@ int memcmp(const void *s1, const void *s2, std::size_t n) {
     return 0;
 }
 
-}
-
 // Halt and catch fire function.
-namespace {
-
-void hcf() {
+static void hcf(void) {
     for (;;) {
+#if defined (__x86_64__)
         asm ("hlt");
+#elif defined (__aarch64__) || defined (__riscv)
+        asm ("wfi");
+#elif defined (__loongarch64)
+        asm ("idle 0");
+#endif
     }
-}
-
 }
 
 bool checkStringEndsWith(const char* str, const char* end)
@@ -158,7 +143,7 @@ struct limine_file* getFile(const char* name)
 {
     struct limine_module_response *module_response = module_request.response;
 
-    if (module_response == nullptr)
+    if (module_response == NULL)
     {
         hcf();
     }
@@ -169,46 +154,27 @@ struct limine_file* getFile(const char* name)
         if (checkStringEndsWith(f->path, name))
             return f;
     }
-
+    
     return NULL;
 }
-
-// The following stubs are required by the Itanium C++ ABI (the one we use,
-// regardless of the "Itanium" nomenclature).
-// Like the memory functions above, these stubs can be moved to a different .cpp file,
-// but should not be removed, unless you know what you are doing.
-extern "C" {
-    int __cxa_atexit(void (*)(void *), void *, void *) { return 0; }
-    void __cxa_pure_virtual() { hcf(); }
-    void *__dso_handle;
-}
-
-// Extern declarations for global constructors array.
-extern void (*__init_array[])();
-extern void (*__init_array_end[])();
 
 // The following will be our kernel's entry point.
 // If renaming kmain() to something else, make sure to change the
 // linker script accordingly.
-extern "C" void kmain() {
+void kmain(void) {
     // Ensure the bootloader actually understands our base revision (see spec).
     if (LIMINE_BASE_REVISION_SUPPORTED == false) {
         hcf();
     }
 
-    // Call global constructors.
-    for (std::size_t i = 0; &__init_array[i] != __init_array_end; i++) {
-        __init_array[i]();
-    }
-
     // Ensure we got a framebuffer.
-    if (framebuffer_request.response == nullptr
+    if (framebuffer_request.response == NULL
      || framebuffer_request.response->framebuffer_count < 1) {
         hcf();
     }
 
     // Fetch the first framebuffer.
-    limine_framebuffer *framebuffer = framebuffer_request.response->framebuffers[0];
+    struct limine_framebuffer *framebuffer = framebuffer_request.response->framebuffers[0];
 
     struct Framebuffer fb;
     {
@@ -240,8 +206,8 @@ extern "C" void kmain() {
     kernel(fb, &font);
 
     // Note: we assume the framebuffer model is RGB with 32-bit pixels.
-    /*for (std::size_t i = 0; i < 100; i++) {
-        volatile std::uint32_t *fb_ptr = static_cast<volatile std::uint32_t *>(framebuffer->address);
+    /*for (size_t i = 0; i < 100; i++) {
+        volatile uint32_t *fb_ptr = framebuffer->address;
         fb_ptr[i * (framebuffer->pitch / 4) + i] = 0xffffff;
     }*/
 
